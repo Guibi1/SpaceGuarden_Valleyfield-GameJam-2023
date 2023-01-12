83ec77c3f40d4e7f8b84515c336ac0b1d9011cb8
Philip Davis <philip29313@hotmail.com>
2023-01-12 00:45:01 -0500

    Player moves

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,18 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+        Vector3 right = transform.right;
+        right.y = 0f;
+
+        PlayerMouvement.instance.setRotation(forward, right);       
+    }
+
+}
