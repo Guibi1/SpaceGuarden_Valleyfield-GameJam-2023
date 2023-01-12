0aa125ff7654ff51a31b1d1904df299d8c461b8d
Emile <emilebouchard99@gmail.com>
2023-01-12 15:41:04 -0500

    tomato

--- Assets/Scripts/Entity/Plants/Tomato.cs
+++ Assets/Scripts/Entity/Plants/Tomato.cs
@@ -10,7 +10,10 @@ public class Tomato : Plant
     public GameObject tomato;
     public float height;
     public float bottom;
-    public float tomatoSize;
+
+    public ParticleSystem particle1;
+    public ParticleSystem particle2;
+    public ParticleSystem particle3;
 
     private void OnEnable()
     {
@@ -22,13 +25,25 @@ public class Tomato : Plant
     
     public override IEnumerator Execute()
     {
+        particle1.Stop();
+        particle2.Stop();
+        particle3.Stop();
+
         yield return new WaitForEndOfFrame();
         StartCoroutine(SimpleRoutines.CustomCurveLerpCoroutine((f) =>
         {
             TopPart.transform.position = new Vector3(TopPart.transform.position.x, f, TopPart.transform.position.z);
         }, TopPart.transform.position.y, bottom, plantData.executeTime, curveCrush));
 
-        
+        StartCoroutine(SimpleRoutines.WaitTime(plantData.executeTime, () =>
+        {
+            particle1.Play();
+            particle2.Play();
+            particle3.Play();
+        }));
+
+
+
     }
     
     public override IEnumerator Preparing()
