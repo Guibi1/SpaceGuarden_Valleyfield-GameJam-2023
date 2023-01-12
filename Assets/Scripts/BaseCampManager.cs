a2fa841c2ae4dddbdbf0ab7fbe6823510731d79b
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-12 18:52:23 -0500

    IHateC#

--- Assets/Scripts/BaseCampManager.cs
+++ Assets/Scripts/BaseCampManager.cs
@@ -34,25 +34,25 @@ public class BaseCampManager : MonoBehaviour
 
         // TODO: Handle turns (spawn ennemies)
 
-        int nbEnemies1 = Math.Floor(4*Math.Sqrt(2.5*currentTurn)+10)
-        int nbEnemies2 = 0;
-        int nbEnemies3 = 0;
+        double nbEnemies1 = Math.Floor(4 * Math.Sqrt(2.5 * currentTurn) + 10);
+        double nbEnemies2 = 0;
+        double nbEnemies3 = 0;
 
         if (currentTurn >= 5)
         {
-            enemies2 = Math.Floor(1.25 * currentTurn);
+            nbEnemies2 = Math.Floor(1.25 * currentTurn);
         }
 
         if (currentTurn >= 15)
         {
-            enemies3 = Math.Floor((X500DistinguishedName/5)-2)
+            nbEnemies3 = Math.Floor((X500DistinguishedName / 5) - 2);
         }
 
         if (currentTurn == 10)
         {
-            enemies1 = 0;
-            enemies2 = 0;
-            enemies13 = 1;
+            nbEnemies1 = 0;
+            nbEnemies2 = 0;
+            nbEnemies3 = 1;
         }
     }
 
