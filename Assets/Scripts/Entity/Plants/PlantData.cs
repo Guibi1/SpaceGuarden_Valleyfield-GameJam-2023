7c5d2c8be8165f1dc37a0bb2b84a9e58291a8789
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-12 17:09:30 -0500

    Added damage to plants and cherry data

--- Assets/Scripts/Entity/Plants/PlantData.cs
+++ Assets/Scripts/Entity/Plants/PlantData.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class PlantData : ScriptableObject
 {
     [Header("Gameplay")]
-    public int cost;
-    public float health;
-    public int timeToShip;
+    public int cost=10;
+    public float health=20f;
+    public float damage = 0f;
+    public int timeToShip=1;
 
     [Header("Animation")]
-    public float intervalBetweenExecute;
-    public float executeTime;
+    public float intervalBetweenExecute=1;
+    public float executeTime=0.25f;
     public float preparingTime;
     public float constructionTime;
     public float radius;
