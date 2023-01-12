1020311096cb92a110176c6e01caa80457d6cf4a
Philip Davis <philip29313@hotmail.com>
2023-01-12 02:08:24 -0500

    consistent movement

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -3,7 +3,9 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMouvement : MonoBehaviour
 {
@@ -11,16 +13,17 @@ public class PlayerMouvement : MonoBehaviour
     public static PlayerMouvement instance;
     private Vector3 cameraVectorForward;
     private Vector3 cameraVectorRight;
-    
 
     [Header ("Settings")]
-    public float speedMultiplier = 50;
+    public float speedMultiplier = 50f;
+    public float spinTime = 8f;
+    public float knockbackForce = 10000f;
 
     [HideInInspector] public Rigidbody rb;
 
     [Header("Misc")]
     [SerializeField] private GameObject sprite;
-
+    public PlayerStates playerState;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,24 +36,49 @@ public class PlayerMouvement : MonoBehaviour
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
         Vector3 targetPos = new Vector3(verticalAxis, 0, horizontalAxis).normalized;
-        rb.velocity = (cameraVectorForward * targetPos.x + targetPos.z *  cameraVectorRight) * speedMultiplier;
+        if (playerState == PlayerStates.Normal)
+        {
+            rb.velocity = (cameraVectorForward * targetPos.x + targetPos.z * cameraVectorRight) * speedMultiplier;
+        }
         sprite.transform.LookAt(cam.transform);
 
         if (horizontalAxis > 0)
         {
-            sprite.GetComponent<SpriteRenderer>().flipX = false;
+            sprite.transform.localScale = new Vector3(1, 1, 1);
         }
         else if (horizontalAxis < 0)
         {
-            sprite.GetComponent<SpriteRenderer>().flipX = true;
+            sprite.transform.localScale = new Vector3(-1, 1, 1);
+
         }
 
-
+        if (Input.GetKeyDown(KeyCode.Mouse0) && playerState == PlayerStates.Normal)
+        {
+            Fire();
+        }
+       
     }
     public void setRotation(Vector3 forward, Vector3 right)
     {
+        
         cameraVectorForward = forward;
         cameraVectorRight = right;
     }
+
+    public void Fire()
+    {
+        playerState = PlayerStates.Knockback;
+        print("Hello, world!");
+        // TODO : Activate animation
+        rb.AddForce(sprite.transform.localScale.x == 1 ?
+            cameraVectorRight * -knockbackForce : // Facing left
+            cameraVectorRight * knockbackForce  // Facing right
+            );
+        StartCoroutine(SimpleRoutines.WaitTime(1, () =>
+        {
+            playerState = PlayerStates.Normal;
+        }));
+    }
+
 
 }
