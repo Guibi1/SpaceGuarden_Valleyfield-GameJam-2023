684bd5526e5f3e15e39df1fec3b7f37fc19e99f2
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-12 18:46:43 -0500

    Added formula for enemy number
    its not good bc it was not balanced with the same standart. The general funtion should work tho

--- Assets/Scripts/BaseCampManager.cs
+++ Assets/Scripts/BaseCampManager.cs
@@ -1,4 +1,6 @@
 using Lean.Pool;
+using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class BaseCampManager : MonoBehaviour
@@ -31,6 +33,27 @@ public class BaseCampManager : MonoBehaviour
         }
 
         // TODO: Handle turns (spawn ennemies)
+
+        int nbEnemies1 = Math.Floor(4*Math.Sqrt(2.5*currentTurn)+10)
+        int nbEnemies2 = 0;
+        int nbEnemies3 = 0;
+
+        if (currentTurn >= 5)
+        {
+            enemies2 = Math.Floor(1.25 * currentTurn);
+        }
+
+        if (currentTurn >= 15)
+        {
+            enemies3 = Math.Floor((X500DistinguishedName/5)-2)
+        }
+
+        if (currentTurn == 10)
+        {
+            enemies1 = 0;
+            enemies2 = 0;
+            enemies13 = 1;
+        }
     }
 
     public void ShipPlant(Plant plant)
