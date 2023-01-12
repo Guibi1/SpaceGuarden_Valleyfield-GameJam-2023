fac5bf1fadb7917f71edda5547f33e1e1c11bdf2
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-12 14:53:33 -0500

    Added SplashDamage prefab

--- Assets/SplashDamage.cs
+++ Assets/SplashDamage.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using Lean.Pool;
+using TMPro;
 using UnityEngine;
 
 public class SplashDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeReference] Camera cam;
+    [SerializeReference] Rigidbody rb;
+    [SerializeReference] TextMeshProUGUI text;
+
+    public void setDamage(int damage)
     {
-        
+        text.text = damage.ToString();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        float maxForce = 2500f;
+        float minForce = 2000f;
+        rb.AddForce(RandomForce(minForce, maxForce), 5000f, RandomForce(minForce, maxForce));
+
+        Color color = text.color;
+        StartCoroutine(SimpleRoutines.LerpCoroutine(1f, 0f, 1.4f,
+            (opacity) =>
+            {
+                color.a = opacity;
+                text.color = color;
+            },
+            () => LeanPool.Despawn(this)
+        ));
+    }
+
     void Update()
     {
-        
+        transform.LookAt(cam.transform);
+    }
+
+    float RandomForce(float min, float max)
+    {
+        return (Random.Range(0, 2) == 0 ? 1 : -1) * Random.Range(min, max);
     }
 }
