44de2497611e708c63cefee9a7f9c43f6a9ad82c
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-12 22:13:12 -0500

    Fixed trophy turn

--- Assets/Scripts/BaseCampManager.cs
+++ Assets/Scripts/BaseCampManager.cs
@@ -27,33 +27,22 @@ public class BaseCampManager : MonoBehaviour
             nextShippement = null;
         }
 
-        if (currentTurn == 10)
+        if (currentTurn == 11)
         {
             OptionsManager.instance.TrophyWon = true;
         }
 
-        // TODO: Handle turns (spawn ennemies)
-
-        int nbEnemies1 = Mathf.FloorToInt(4f * Mathf.Sqrt(2.5f * currentTurn) + 10f);
-        int nbEnemies2 = 0;
-        int nbEnemies3 = 0;
-
-        if (currentTurn >= 5)
-        {
-            nbEnemies2 = Mathf.FloorToInt(1.25f * currentTurn);
-        }
-
-        if (currentTurn >= 15)
-        {
-            nbEnemies3 = Mathf.FloorToInt((currentTurn / 5f) - 2f);
-        }
-
         if (currentTurn == 10)
         {
-            nbEnemies1 = 0;
-            nbEnemies2 = 0;
-            nbEnemies3 = 1;
+            //spawn big #3 (scale * 3?)
+            return;
         }
+
+        int nbEnemies1 = Mathf.FloorToInt(4f * Mathf.Sqrt(2.5f * currentTurn) + 10f);
+        int nbEnemies2 = currentTurn >= 5 ? Mathf.FloorToInt(1.25f * currentTurn) : 0;
+        int nbEnemies3 = currentTurn >= 15 ? Mathf.FloorToInt((currentTurn / 5f) - 2f) : 0;
+
+        // TODO: Handle turns (spawn ennemies)
     }
 
     public void ShipPlant(Plant plant)
